2c898327b1955e5f885bdc4917bcd2bbde27a1a6
simon <simon.emmott@yahoo.co.uk>
2019-01-13 11:59:01 +0000

    Added entity Cache and LinkedCache

--- PythonEntities/src/entities/cache.py
+++ PythonEntities/src/entities/cache.py
@@ -12,7 +12,7 @@ class CacheError(Exception):
 
 class Cache(object):
 
-    def __init__(self):
+    def __init__(self, *args, **kw):
         self._cache = {}
         
     def _ancestor_cache(self, ancestor):
@@ -57,140 +57,59 @@ class Cache(object):
     def list(self, entity):
         ancestor_entity = entities.get_ancestor_entity(entity)
         ancestor_cache = self._ancestor_cache(ancestor_entity)
-        if entity == ancestor_entity:
-            return ancestor_cache.values()
         lst = []
         for e in ancestor_cache.values():
             if isinstance(e, entity):
                 lst.append(e)
         return lst
-
-class CommitCache(object):
-
-    def __init__(self):
-        self._cache = {}
-        
-    def _ancestor_cache(self, ancestor):
-        ancestor_cache = self._cache.get(ancestor)
-        if not ancestor_cache:
-            ancestor_cache = {}
-            self._cache[ancestor] = ancestor_cache
-        return ancestor_cache
     
-    def transaction(self, old, new):
-        if old:
-            cref = old._cref_
-        else:
-            cref = new._cref_
-        s = cref+':'+json.dumps(classUtil.to_dict(diff.compare(old, new)))
-        return hashlib.md5(s.encode('utf-8')).hexdigest()
-    
+    def list_types(self):
+        return self._cache.keys()
+
+class LinkedCache(Cache):
+    def __init__(self, source, **kw):
+        Cache.__init__(self, **kw)
+        self.source = source
+        self.removed = []
+
     def put(self, obj):
-        if not hasattr(obj, '_cref_'):
-            setattr(obj, '_cref_', 'new')
-                     
-        ancestor_cache = self._ancestor_cache(entities.get_ancestor_entity(obj.__class__))
-        pk = entities.get_pk(obj)
-        cached = ancestor_cache.get(pk)
-        if not cached or cached._cref_ == obj._cref_:
-            if not cached:
-                obj._cref_ = self.transaction(None, obj)
-            else:
-                obj._cref_ = self.transaction(cached, obj)
-            ancestor_cache[pk] = obj
-            return obj
-            
-        else:
-            raise CacheError("Commit references don't match")
-        
-    def get(self, entity, *keys):
-        if len(keys) == 0:
-            raise KeyError('Unable to fetch entities of type %s without a key' % entities.get_entity_name(entity))
-        elif len(keys) == 1:
-            key = keys[0]
-            if not isinstance(key, tuple):
-                key = (key,)
-        else:
-            key = ()
-            for k in keys:
-                if isinstance(k, tuple):
-                    key = key + k
-                else:
-                    key = key + (k,)
-
-        ancestor_cache = self._ancestor_cache(entities.get_ancestor_entity(entity))
-        e = ancestor_cache.get(key)
-        if not e:
-            raise KeyError('No entity of type %s found with key %s' % (entity, key))
-        return e
+        if obj in self.removed:
+            self.removed.remove(obj)
+        return Cache.put(self, obj)
     
-    def remove(self, obj):
-        ancestor_cache = self._ancestor_cache(entities.get_ancestor_entity(obj.__class__))
-        pk = entities.get_pk(obj)
-        cached = ancestor_cache[pk]
-        if cached._cref_ != obj._cref_:
-            raise CacheError("Commit references don't match")
-        obj._cref_ = self.transaction(cached, None)
-        del ancestor_cache[pk]
-        return obj
-        
-    def list(self, entity):
-        ancestor_entity = entities.get_ancestor_entity(entity)
-        ancestor_cache = self._ancestor_cache(ancestor_entity)
-        if entity == ancestor_entity:
-            return ancestor_cache.values()
-        lst = []
-        for e in ancestor_cache.values():
-            if isinstance(e, entity):
-                lst.append(e)
-        return lst
-
-def get_entity(tpl):
-    return tpl[0]
-
-def get_commit_ref(tpl):
-    return tpl[1]
-
-def commit_ref(trans, cref):
-    s = cref+':'+json.dumps(classUtil.to_dict(trans))
-    return hashlib.md5(s.encode('utf-8')).hexdigest()
-    
-
-class ServerCache(CommitCache):
-        
-    def session(self):
-        local = threading.local()
-        if hasattr(local, 'session'):
-            return local.session
-        s = SessionCache(self)
-        local.session = s
-        return s
-            
-class SessionCache(Cache):
-    def __init__(self, server):
-        self._server = server
-        
     def get(self, entity, *keys):
         try:
             return Cache.get(self, entity, *keys)
-        except(KeyError):
-            e = copy.deepcopy(self._server.get(entity, *keys))
-            self.put(e)
-            return e
-        
-    def populate(self, entity):
-        lst = Cache.list(self, entity)
-        for e in itertools.starmap(copy.deepcopy, self._server.list(entity)):
-            if not e in lst:
-                lst.append(e)
-                self.put(e)
-        return lst
-
+        except KeyError:
+            hit = self.source.get(entity, *keys)
+            if hit not in self.removed:
+                Cache.put(self, hit)
+                return hit
+    
+    def remove(self, obj):
+        if obj in self.removed:
+            return obj
+        self.removed.append(obj)
+        return Cache.remove(self, obj)
+    
     def list(self, entity):
-        lst = Cache.list(self, entity)
-        for e in self._server.list(entity):
-            if not e in lst:
-                lst.append(e)
-        return lst
-
+        cache_lst =  Cache.list(self, entity)
+        for item in self.source.list(entity):
+            if item not in cache_lst and item not in self.removed:
+                cache_lst.append(item)
+        return cache_lst
+    
+    def push(self):
+        for cls in self.list_types():
+            print('Pushing caches for type %s' % cls)
+            for obj in self.list(cls):
+                self.source.put(obj)
+        for removed in self.removed:
+            self.source.remove(removed)
+        self.clear()
+        
+    def clear(self):
+        self._cache = {}
+        self.removed = []
+        
         
--- PythonEntities/test/cache_test.py
+++ PythonEntities/test/cache_test.py
@@ -5,7 +5,7 @@ Created on 16 Dec 2018
 '''
 
 import entities, itertools
-from entities.cache import Cache, CommitCache, ServerCache, get_entity, commit_ref
+from entities.cache import Cache, LinkedCache
 import unittest
 from test_entities import *
                 
@@ -118,123 +118,195 @@ class TestCache(unittest.TestCase):
         self.assertEqual(1, len(cache.list(EntityA)))
         
         
-class TestCommitCache(unittest.TestCase):
+class TestLinkedCache(unittest.TestCase):
     
-    def test_new_cache(self):
-        cache = CommitCache()
-        self.assertNotEqual(None, cache)
-        self.assertTrue(isinstance(cache, CommitCache))
-
-    def test_ancestor_cache(self):
-        cache = CommitCache() 
-         
-        eA1 = EntityA(id=1, name='EntityA 1') 
-        cache.put(eA1)
+    def test_new_linked_cache(self):
+        source = Cache()
         
-        root_cache = cache._ancestor_cache(EntityA)
-        self.assertNotEqual(None, root_cache)
-        self.assertEqual(1, len(root_cache))
-        
-    def test_get(self): 
-        cache = CommitCache() 
-         
         eA1 = EntityA(id=1, name='EntityA 1') 
         eA2 = EntityA(id=2, name='EntityA 2') 
-        cache.put(eA1)
-        cache.put(eA2)
-        self.assertEqual(eA1, cache.get(EntityA, 1))
-        self.assertEqual(eA1, cache.get(EntityA, (1,)))
-        self.assertEqual(eA2, cache.get(EntityA, 2))
-        self.assertEqual(eA2, cache.get(EntityA, (2,)))
-             
+        source.put(eA1)
+        source.put(eA2) 
+        
         eB1 = EntityB(package='pack', name='EntityB 1') 
         eB2 = EntityB(package='pack', name='EntityB 2') 
-        cache.put(eB1)
-        cache.put(eB2)
-        self.assertEqual(eB1, cache.get(EntityB, 'pack', 'EntityB 1'))
-        self.assertEqual(eB1, cache.get(EntityB, ('pack', 'EntityB 1')))
-        self.assertEqual(eB2, cache.get(EntityB, 'pack', 'EntityB 2'))
-        self.assertEqual(eB2, cache.get(EntityB, ('pack', 'EntityB 2')))
-        
-        eABC3 = EntityABC(id=3, name='EntityABC 3') 
-        eABC4 = EntityABC(id=4, name='EntityABC 4') 
-        cache.put(eABC3)
-        cache.put(eABC4)
-        self.assertEqual(eABC3, cache.get(EntityABC, 3))
-        self.assertEqual(eABC3, cache.get(EntityABC, (3,)))
-        self.assertEqual(eABC4, cache.get(EntityABC, 4))
-        self.assertEqual(eABC4, cache.get(EntityABC, (4,)))
-        self.assertEqual(eABC3, cache.get(EntityA, 3))
-        self.assertEqual(eABC3, cache.get(EntityA, (3,)))
-        self.assertEqual(eABC4, cache.get(EntityA, 4))
-        self.assertEqual(eABC4, cache.get(EntityA, (4,)))
+        source.put(eB1)
+        source.put(eB2)
 
-    def test_list(self): 
-        cache = CommitCache() 
-         
+        linked = LinkedCache(source)
+        
+        self.assertNotEqual(None, linked)
+        
+    def test_put_linked_cache(self):
+        source = Cache()
+        
         eA1 = EntityA(id=1, name='EntityA 1') 
         eA2 = EntityA(id=2, name='EntityA 2') 
-        cache.put(eA1)
-        cache.put(eA2) 
-                    
+        source.put(eA1)
+        source.put(eA2) 
+        
         eB1 = EntityB(package='pack', name='EntityB 1') 
         eB2 = EntityB(package='pack', name='EntityB 2') 
-        cache.put(eB1)
-        cache.put(eB2)
-        
-        eABC3 = EntityABC(id=3, name='EntityABC 3') 
-        eABC4 = EntityABC(id=4, name='EntityABC 4') 
-        cache.put(eABC3)
-        cache.put(eABC4)
+        source.put(eB1)
+        source.put(eB2)
 
-        self.assertEqual(4, len(cache.list(EntityA)))
-        self.assertEqual(2, len(cache.list(EntityB)))
-        self.assertEqual(2, len(cache.list(EntityAB)))
-        self.assertEqual(2, len(cache.list(EntityABC)))
+        linked = LinkedCache(source)
         
-        self.assertTrue(eA1 in cache.list(EntityA))
-        self.assertTrue(eA2 in cache.list(EntityA))
-        self.assertTrue(eABC3 in cache.list(EntityA))
-        self.assertTrue(eABC4 in cache.list(EntityA))
+        eA3 = EntityA(id=3, name='EntityA 3')
+        linked.put(eA3)
         
-        self.assertTrue(eB1 in cache.list(EntityB))
-        self.assertTrue(eB2 in cache.list(EntityB))
-
-        self.assertTrue(eABC3 in cache.list(EntityAB))
-        self.assertTrue(eABC4 in cache.list(EntityAB))
-             
-        self.assertTrue(eABC3 in cache.list(EntityABC))
-        self.assertTrue(eABC4 in cache.list(EntityABC))
+        self.assertTrue(eA3 in linked.list(EntityA))
+        self.assertFalse(eA3 in source.list(EntityA))
         
-    def test_remove(self): 
-        cache = CommitCache() 
-         
+    def test_get_linked_cache(self):
+        source = Cache()
+        
         eA1 = EntityA(id=1, name='EntityA 1') 
         eA2 = EntityA(id=2, name='EntityA 2') 
-        eA1 = cache.put(eA1)
-        eA2 = cache.put(eA2) 
-                            
-        self.assertNotEqual(None, cache.get(EntityA, 1))
-        cache.remove(eA1)
+        source.put(eA1)
+        source.put(eA2) 
+        
+        eB1 = EntityB(package='pack', name='EntityB 1') 
+        eB2 = EntityB(package='pack', name='EntityB 2') 
+        source.put(eB1)
+        source.put(eB2)
+
+        linked = LinkedCache(source)
+        
+        got_a1 = linked.get(EntityA, 1)
+        
+        self.assertEqual(got_a1, eA1)
+        self.assertTrue(eA1 in linked.list(EntityA))
+        
+    def test_remove_linked_cache(self):
+        source = Cache()
+        
+        eA1 = EntityA(id=1, name='EntityA 1') 
+        eA2 = EntityA(id=2, name='EntityA 2') 
+        source.put(eA1)
+        source.put(eA2) 
+        
+        eB1 = EntityB(package='pack', name='EntityB 1') 
+        eB2 = EntityB(package='pack', name='EntityB 2') 
+        source.put(eB1)
+        source.put(eB2)
+
+        linked = LinkedCache(source)
+        
+        self.assertTrue(eA1 in linked.list(EntityA))
+        self.assertTrue(eA1 in source.list(EntityA))
+        
         try:
-            cache.get(EntityA, 1)
-            self.fail('Key error expected')
-        except(KeyError):
+            linked.remove(eA1)
+            self.fail("Remove should fail if the instance to be removed is not in the cache")
+        except KeyError:
             pass
-        except:
-            self.fail('Key error expected')
-            
-        self.assertEqual(1, len(cache.list(EntityA)))
-
-
-class TestServerCache(unittest.TestCase):
-    
-    def test_new_server_cache(self):
-        sc = ServerCache()
-        self.assertNotEqual(None, sc)
-        self.assertTrue(isinstance(sc, ServerCache))
         
-    
+        got_a1 = linked.get(EntityA, 1)
+
+        try:
+            linked.remove(eA1)
+        except KeyError:
+            self.fail("Remove should not fail if the item to be removed has been fetched into the cache")
+            
+        self.assertFalse(eA1 in linked.list(EntityA))
+        self.assertTrue(eA1 in source.list(EntityA))
+
+        
+    def test_list_linked_cache(self):
+        source = Cache()
+        
+        eA1 = EntityA(id=1, name='EntityA 1') 
+        eA2 = EntityA(id=2, name='EntityA 2') 
+        source.put(eA1)
+        source.put(eA2) 
+        
+        eB1 = EntityB(package='pack', name='EntityB 1') 
+        eB2 = EntityB(package='pack', name='EntityB 2') 
+        source.put(eB1)
+        source.put(eB2)
+
+        linked = LinkedCache(source)
+        
+        eA3 = EntityA(id=3, name='EntityA 3')
+        linked.put(eA3)
+        
+        self.assertTrue(eA1 in linked.list(EntityA))
+        self.assertTrue(eA2 in linked.list(EntityA))
+        self.assertTrue(eA3 in linked.list(EntityA))
+        self.assertTrue(eA1 in source.list(EntityA))
+        self.assertTrue(eA2 in source.list(EntityA))
+        self.assertFalse(eA3 in source.list(EntityA))
+        
+    def test_push_linked_cache(self):
+        source = Cache()
+        
+        eA1 = EntityA(id=1, name='EntityA 1') 
+        eA2 = EntityA(id=2, name='EntityA 2') 
+        source.put(eA1)
+        source.put(eA2) 
+        
+        eB1 = EntityB(package='pack', name='EntityB 1') 
+        eB2 = EntityB(package='pack', name='EntityB 2') 
+        source.put(eB1)
+        source.put(eB2)
+
+        linked = LinkedCache(source)
+        
+        eA3 = EntityA(id=3, name='EntityA 3')
+        linked.put(eA3)
+        
+        got_a1 = linked.get(EntityA, 1)
+        linked.remove(got_a1)
+        
+        self.assertFalse(eA1 in linked.list(EntityA))
+        self.assertTrue(eA2 in linked.list(EntityA))
+        self.assertTrue(eA3 in linked.list(EntityA))
+        self.assertTrue(eA1 in source.list(EntityA))
+        self.assertTrue(eA2 in source.list(EntityA))
+        self.assertFalse(eA3 in source.list(EntityA))
+        
+        linked.push()
+        self.assertFalse(eA1 in source.list(EntityA))
+        self.assertTrue(eA2 in source.list(EntityA))
+        self.assertTrue(eA3 in source.list(EntityA))
+        
+    def test_clear_linked_cache(self):
+        source = Cache()
+        
+        eA1 = EntityA(id=1, name='EntityA 1') 
+        eA2 = EntityA(id=2, name='EntityA 2') 
+        source.put(eA1)
+        source.put(eA2) 
+        
+        eB1 = EntityB(package='pack', name='EntityB 1') 
+        eB2 = EntityB(package='pack', name='EntityB 2') 
+        source.put(eB1)
+        source.put(eB2)
+
+        linked = LinkedCache(source)
+        
+        eA3 = EntityA(id=3, name='EntityA 3')
+        linked.put(eA3)
+        
+        got_a1 = linked.get(EntityA, 1)
+        linked.remove(got_a1)
+        
+        self.assertFalse(eA1 in linked.list(EntityA))
+        self.assertTrue(eA2 in linked.list(EntityA))
+        self.assertTrue(eA3 in linked.list(EntityA))
+        self.assertTrue(eA1 in source.list(EntityA))
+        self.assertTrue(eA2 in source.list(EntityA))
+        self.assertFalse(eA3 in source.list(EntityA))
+        
+        linked.clear()
+        
+        self.assertTrue(eA1 in linked.list(EntityA))
+        self.assertTrue(eA2 in linked.list(EntityA))
+        self.assertFalse(eA3 in linked.list(EntityA))
+        self.assertTrue(eA1 in source.list(EntityA))
+        self.assertTrue(eA2 in source.list(EntityA))
+        self.assertFalse(eA3 in source.list(EntityA))
 
 
 if __name__ == "__main__":
